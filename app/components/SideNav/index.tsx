2bfdb2111c8cf6b40fa4d19f8f8c79d16b3236e6
Nishit Jha <jhanishitback@gmail.com>
2022-02-28 18:55:57 +0530

    things

--- app/components/SideNav/index.tsx
+++ app/components/SideNav/index.tsx
@@ -129,4 +129,111 @@ export default function SideNav() {
       </>
     );
   }
+
+  return (
+    <>
+      <Hamburger
+        onClick={() => {
+          setSideNavOpen(true);
+        }}
+        className={styles.hamburger}
+      />
+      <div
+        className={cx({
+          [styles.overlay]: true,
+          [styles['overlay-open']]: sideNavOpen,
+        })}
+      ></div>
+
+      <div
+        className={cx({
+          [styles.sideNav]: true,
+          [styles['sideNav-open']]: sideNavOpen,
+        })}
+        ref={sideNavRef}
+      >
+        <div className={styles.sideNav__body}>
+          <div
+            onClick={() => {
+              setSideNavOpen(false);
+            }}
+            className={styles.sideNav__body__logo}
+          >
+            <Logo />
+          </div>
+
+          <div className={styles.sideNav__body__links}>
+            <Link href="/" passHref>
+              <div
+                className={cx({
+                  [styles.sideNav__body__links__link]: true,
+                  [styles['sideNav__body__links__link--active']]:
+                    router.pathname === '/',
+                })}
+              >
+                <div className={styles.sideNav__body__links__link__icon}>
+                  <Icon
+                    color="#9e9e9e"
+                    width={32}
+                    height={32}
+                    icon="mdi-light:home"
+                  />
+                </div>
+
+                <div className={styles.sideNav__body__links__link__label}>
+                  Home
+                </div>
+              </div>
+            </Link>
+
+            <Link href="/projects" passHref>
+              <div
+                className={cx({
+                  [styles.sideNav__body__links__link]: true,
+                  [styles['sideNav__body__links__link--active']]:
+                    router.pathname === '/projects',
+                })}
+              >
+                <div className={styles.sideNav__body__links__link__icon}>
+                  <Icon
+                    color="#9e9e9e"
+                    width={32}
+                    height={32}
+                    icon="mdi-light:star"
+                  />
+                </div>
+
+                <div className={styles.sideNav__body__links__link__label}>
+                  My Projects
+                </div>
+              </div>
+            </Link>
+
+            <Link href="/guestbook" passHref>
+              <div
+                className={cx({
+                  [styles.sideNav__body__links__link]: true,
+                  [styles['sideNav__body__links__link--active']]:
+                    router.pathname === '/guestbook',
+                })}
+              >
+                <div className={styles.sideNav__body__links__link__icon}>
+                  <Icon
+                    color="#9e9e9e"
+                    width={32}
+                    height={32}
+                    icon="mdi-light:pencil"
+                  />
+                </div>
+
+                <div className={styles.sideNav__body__links__link__label}>
+                  The Guestbook
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
