7c5f828f63e4a3b2bc29b137818a469f11f7c103
Nishit Jha <jhanishitback@gmail.com>
2022-03-03 19:51:23 +0530

    hmm

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -9,36 +9,23 @@ export default function Document() {
           rel="stylesheet"
         />
 
-        <title>Arcula</title>
-        <meta name="title" content="Arcula" />
-        <meta name="description" content="Treasure your privacy with Arcula." />
+        <meta name="title" content="Nishit Jha" />
+        <meta name="description" content="My portfolio" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Arcula" />
-        <meta property="og:url" content="https://arcula.co/" />
-        <meta property="og:title" content="Arcula" />
-        <meta
-          property="og:description"
-          content="Treasure your privacy with Arcula."
-        />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:site_name" content="Nishit Jha" />
+        <meta property="og:url" content="https://nishitjha.vercel.app" />
+        <meta property="og:title" content="Nishit Jha" />
+        <meta property="og:description" content="My portfolio" />
+        <meta property="og:image" content="./public/banner.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="628" />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://arcula.co/" />
-        <meta property="twitter:title" content="Arcula" />
-        <meta
-          property="twitter:description"
-          content="Treasure your privacy with Arcula."
-        />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:url" content="https://nishitjha.vercel.app" />
+        <meta property="twitter:title" content="Nishit Jha" />
+        <meta property="twitter:description" content="My portfolio" />
+        <meta property="twitter:image" content="./public/banner.png" />
       </Head>
       <body>
         <Main />
