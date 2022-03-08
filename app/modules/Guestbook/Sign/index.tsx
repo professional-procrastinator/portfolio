4edd5dc4fa9a56aec33294e29f50180feacbfa07
Nishit Jha <jhanishitback@gmail.com>
2022-03-08 13:53:51 +0530

    super pro 2021 free react videos

--- app/modules/Guestbook/Sign/index.tsx
+++ app/modules/Guestbook/Sign/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
 import TextField from '../../../components/TextField';
 import TypeWriter from '../../../components/TypeWriter';
 import styles from '../../../styles/shared/popup.module.scss';
+import Loader from '../../../components/Loader';
 
 export default function Sign() {
-  const [message, setMessage] = useState('');
+  const { data, status } = useSession();
+
   return (
     <div className={styles.popup}>
       <div className={styles.popup__banner}>
@@ -17,20 +20,43 @@ export default function Sign() {
         />
       </div>
 
-      <div className={styles.popup__content}>
-        <div className={styles.popup__content__heading}>
-          <TypeWriter words={['Some words for the chef?']} />
-        </div>
-
-        <TextField
-          textarea
-          label={'Your Message'}
-          value={message}
-          setValue={setMessage}
-          limit={200}
-          className={styles.popup__content__textarea}
-        />
-      </div>
+      <>{status === 'loading' ? <SignLoading /> : <SignForm />}</>
     </div>
   );
 }
+
+const SignForm = () => {
+  const [message, setMessage] = useState('');
+  return (
+    <div className={styles.popup__content}>
+      <div className={styles.popup__content__heading}>
+        <TypeWriter words={['Some words for the chef?']} />
+      </div>
+
+      <TextField
+        textarea
+        label={'Your Message'}
+        value={message}
+        setValue={setMessage}
+        limit={200}
+        className={styles.popup__content__textarea}
+      />
+    </div>
+  );
+};
+
+const SignLoading = () => {
+  return (
+    <div className={styles.popup__loading}>
+      <Loader
+        containerStyles={{
+          margin: 'auto',
+        }}
+        mainStyles={{
+          borderColor: 'var(--primary-light)',
+          borderRightColor: 'transparent',
+        }}
+      />
+    </div>
+  );
+};
